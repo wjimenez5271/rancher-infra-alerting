d1496a8245d15c4d1918652601f27e882b35898d
William Jimenez <william@rancher.com>
2017-03-17 11:40:00 -0700

    use tornado for handling WSGI requests

--- server/main.py
+++ server/main.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, abort
 import logging
 import syslog_client
+import os
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 
-LOG_LEVEL = 'DEBUG'
-SYSLOG_HOST = 'localhost'
+
+SYSLOG_HOST = os.getenv("SYSLOG_HOST", "localhost")
+BIND_PORT = int(os.getenv("BIND_PORT", 5050))
+LOG_LEVEL = os.getenv("LOG_LEVEL",'DEBUG')
 
 app = Flask(__name__)
 
@@ -12,12 +18,17 @@ def init_logger():
     global LOG_LEVEL
     log = logging.getLogger('infra-monitor-server')
     log.setLevel(LOG_LEVEL)
-    log.propagate = False
+    log.propagate = True
     stderr_logs = logging.StreamHandler()
     stderr_logs.setLevel(getattr(logging, LOG_LEVEL))
     stderr_logs.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
     log.addHandler(stderr_logs)
+    # Format tornado's logs
+    logging.getLogger('tornado').addHandler(stderr_logs)
+    logging.getLogger('tornado').setLevel(getattr(logging, LOG_LEVEL))
+
     return log
+
 
 def write_to_syslog(payload):
     sclient.warn(payload)
@@ -33,7 +44,7 @@ def handle_alert(alert_struct):
         check_name=check_name,
         check_desc=description
     )
-    log.debug("Sending the following to syslog {0}".format(message))
+    log.info("Sending the following to syslog {0}".format(message))
     write_to_syslog(message)
 
 
@@ -51,6 +62,7 @@ def report_alert():
     handle_alert(request.json)
     return 'OK'
 
+
 @app.route("/health", methods=['GET'])
 def health():
     '''
@@ -63,4 +75,6 @@ if __name__ == "__main__":
     log = init_logger()
     sclient = syslog_client.Syslog(host=SYSLOG_HOST)
     log.info('Starting server')
-    app.run(host="0.0.0.0", port=5050, debug=False)
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(BIND_PORT)
+    IOLoop.instance().start()
